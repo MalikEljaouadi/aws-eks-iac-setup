7ea51d786a04d85857469de263709ef8ff44a7e5
Malik Eljaouadi <malekabidi25@gmail.com>
2024-07-08 21:51:07 +0200

    🚀 deploying the metrics server

--- metrics-server.tf
+++ metrics-server.tf
@@ -1,26 +1,13 @@
-resource "helm_release" "example" {
-  name       = "my-redis-release"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "redis"
-  version    = "6.0.1"
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
 
   values = [
-    "${file("values.yaml")}"
+    "${file("${path.module}/k8s/values/metrics-server.yaml")}"
   ]
 
-  set {
-    name  = "cluster.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "metrics.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "service.annotations.prometheus\\.io/port"
-    value = "9127"
-    type  = "string"
-  }
+  depends_on = [aws_eks_node_group.general]
 }
